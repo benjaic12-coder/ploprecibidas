8fdfcc1bd72978a1c2e7dbd42ab5909f30b3d539
benjaic12-coder <benja.ic12@gmail.com>
2026-07-06 19:11:18 -0300

    Corregir imagenes Plopcito y catalogo v25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='plop-recibidas-v23-flat';
+const CACHE='plop-recibidas-v25-assets';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))));
